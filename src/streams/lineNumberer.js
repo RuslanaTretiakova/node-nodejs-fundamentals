d87e431865daa515724a9cd4b925cc1125a005b7
Ruslana <tretiakovaruslana7@gmail.com>
2026-03-08 21:06:33 +0100

    feat: add

--- src/streams/lineNumberer.js
+++ src/streams/lineNumberer.js
@@ -1,8 +1,23 @@
-const lineNumberer = () => {
-  // Write your code here
-  // Read from process.stdin
-  // Use Transform Stream to prepend line numbers
-  // Write to process.stdout
+import { Transform } from "node:stream";
+
+export const lineNumberer = () => {
+  let line = 1;
+
+  const transformer = new Transform({
+    readableObjectMode: false,
+    writableObjectMode: false,
+
+    transform(chunk, encoding, callback) {
+      const lines = chunk.toString().split("\n");
+      const numbered = lines
+        .map((l) => `${line++} | ${l}`)
+        .join("\n");
+
+      callback(null, numbered);
+    }
+  });
+
+  process.stdin.pipe(transformer).pipe(process.stdout);
 };
 
 lineNumberer();
